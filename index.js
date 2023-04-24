5d3d238fdab8e0ed8b673fb73bc124a87e44bac0
Laureano Rodriguez <laure.jd0101@gmail.com>
2023-04-24 16:54:21 -0300

    Routing con Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('<h1>Hola mi server en express</h1>');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('<h1>Hola, soy una nueva ruta</h1>');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });
