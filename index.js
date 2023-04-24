8327809777630386918add5541f4d44b445a0923
Laureano Rodriguez <laure.jd0101@gmail.com>
2023-04-24 18:48:24 -0300

    GET: parámetros query e incorporamos la librería Faker al proyecto

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
@@ -11,16 +13,21 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('<h1>Yo soy un filter</h1>');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -60,14 +67,15 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  res.json([
-    {
-      name: 'User 1',
-    },
-    {
-      name: 'User 2',
-    },
-  ]);
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
 });
 
 app.get('/users/:id', (req, res) => {
