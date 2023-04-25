80399d7dfed9d72bfb480bc6ebda02b89947fc3b
Laureano Rodriguez <laure.jd0101@gmail.com>
2023-04-24 22:28:34 -0300

    POST: método para crear, y usamos el cliente de APIs Insomnia

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('<h1>Hola mi server en express</h1>');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
+
 const productRouter = require('./products.router');
 const categoryRouter = require('./categories.router');
 const userRouter = require('./users.router');
 
 function routerApi(app) {
-  app.use('/api/products', productRouter);
-  app.use('/api/categories', categoryRouter);
-  app.use('/api/users', userRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productRouter);
+  router.use('/categories', categoryRouter);
+  router.use('/users', userRouter);
 }
 
 module.exports = routerApi;
